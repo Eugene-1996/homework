cd2fa800726b31b76f85d265cf063883a5354285
gekasamarin1@gmail.com <gekasamarin1@gmail.com>
2023-05-01 11:59:46 +0200

    HW13

--- hws2/src/s2-homeworks/hw13/HW13.tsx
+++ hws2/src/s2-homeworks/hw13/HW13.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import s2 from '../../s1-main/App.module.css'
 import s from './HW13.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
@@ -24,7 +24,7 @@ const HW13 = () => {
         const url =
             x === null
                 ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
-                : 'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test'
+                : 'https://samurai.it-incubator.io/api/3.0/homework/test'
 
         setCode('')
         setImage('')
@@ -32,15 +32,44 @@ const HW13 = () => {
         setInfo('...loading')
 
         axios
-            .post(url, {success: x})
+            .post(url, { success: x })
+
             .then((res) => {
-                setCode('Код 200!')
-                setImage(success200)
+                console.log(res.status)
+                if (res.status === 200) {
+                    setCode('Код 200!')
+                    setImage(success200)
+                    setText('...всё ок)')
+                    setInfo('код 200 - обычно означает что скорее всего всё ок)')
+                }
+
+
                 // дописать
+
 
             })
             .catch((e) => {
+                console.log(e.response.status)
+                console.log(e, 'erorr')
                 // дописать
+                if (e.response.status === 500) {
+                    setCode('Ошибка 500!')
+                    setImage(error500)
+                    setText('эмитация ошибки на сервере')
+                    setInfo('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                }
+                else if (e.response.status === 400) {
+                    setCode('Ошибка 400!')
+                    setImage(error400)
+                    setText('Ты не отправил success в body вообще!')
+                    setInfo('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                }
+                else if (e.response.status === 0) {
+                    setCode('Error')
+                    setImage(errorUnknown)
+                    setText('Network Error!')
+                    setInfo('AxiosError')
+                }
 
             })
     }
@@ -55,7 +84,7 @@ const HW13 = () => {
                         id={'hw13-send-true'}
                         onClick={send(true)}
                         xType={'secondary'}
-                        // дописать
+                    // дописать
 
                     >
                         Send true
@@ -64,7 +93,7 @@ const HW13 = () => {
                         id={'hw13-send-false'}
                         onClick={send(false)}
                         xType={'secondary'}
-                        // дописать
+                    // дописать
 
                     >
                         Send false
@@ -73,7 +102,7 @@ const HW13 = () => {
                         id={'hw13-send-undefined'}
                         onClick={send(undefined)}
                         xType={'secondary'}
-                        // дописать
+                    // дописать
 
                     >
                         Send undefined
@@ -82,7 +111,7 @@ const HW13 = () => {
                         id={'hw13-send-null'}
                         onClick={send(null)} // имитация запроса на не корректный адрес
                         xType={'secondary'}
-                        // дописать
+                    // дописать
 
                     >
                         Send null
@@ -91,7 +120,7 @@ const HW13 = () => {
 
                 <div className={s.responseContainer}>
                     <div className={s.imageContainer}>
-                        {image && <img src={image} className={s.image} alt="status"/>}
+                        {image && <img src={image} className={s.image} alt="status" />}
                     </div>
 
                     <div className={s.textContainer}>
